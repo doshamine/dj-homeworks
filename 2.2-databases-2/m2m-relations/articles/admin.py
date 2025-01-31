1c6f681191fbd08a2f3caa38f89a30a3d58ba387
doshamine <doshamine@gmail.com>
2025-01-31 19:35:49 +0600

    my first commit

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,41 @@
-from django.contrib import admin
+from pprint import pprint
 
-from .models import Article
+from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+
+from .models import Article, Tag, Scope
+
+class ArticleScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        mains_num = 0
+        pprint(self.forms)
+        for form in self.forms:
+            # В form.cleaned_data будет словарь с данными
+            # каждой отдельной формы, которые вы можете проверить
+            if form.cleaned_data['is_main']:
+                mains_num += 1
+            # вызовом исключения ValidationError можно указать админке о наличие ошибки
+            # таким образом объект не будет сохранен,
+            # а пользователю выведется соответствующее сообщение об ошибке
+        if mains_num == 0:
+            raise ValidationError('Выберите главный тэг')
+        elif mains_num > 1:
+            raise ValidationError('Главный тэг может быть только один')
+
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+
+class ArticleScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ArticleScopeInlineFormset
+    extra = 0
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ArticleScopeInline]
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass
--- 2.2-databases-2/m2m-relations/articles/views.py
+++ 2.2-databases-2/m2m-relations/articles/views.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 from django.shortcuts import render
 
 from articles.models import Article
@@ -5,10 +7,8 @@ from articles.models import Article
 
 def articles_list(request):
     template = 'articles/news.html'
-    context = {}
+    articles = Article.objects.all()
 
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = '-published_at'
+    context = {'object_list': articles}
 
     return render(request, template, context)
