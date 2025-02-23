cfb441ed82c98b7620210a760d47c7379ab00ddf
doshamine <doshamine@gmail.com>
2025-02-23 18:30:35 +0600

    first version

--- 3.4-django-testing/django_testing/students/serializers.py
+++ 3.4-django-testing/django_testing/students/serializers.py
@@ -1,10 +1,28 @@
 from rest_framework import serializers
 
-from students.models import Course
+from students.models import Course, Student
 
+
+class StudentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Student
+        fields = ("id", "name", "birth_date")
 
 class CourseSerializer(serializers.ModelSerializer):
+    students = StudentSerializer(many=True)
 
     class Meta:
         model = Course
         fields = ("id", "name", "students")
+
+    def create(self, validated_data):
+        students_data = validated_data.pop('students')
+
+        course = Course.objects.create(**validated_data)
+
+        for student_data in students_data:
+            student_json = {**student_data, 'course': course}
+            Student.objects.create(**student_json)
+
+        return course
--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -39,10 +39,91 @@ def test_retrieve_courses(client, student_factory, course_factory):
     students = student_factory(_quantity=5)
     courses = course_factory(_quantity=3, students=students)
     course_id = courses[0].id
+    course_name = courses[0].name
 
     response = client.get('/api/v1/courses/', {'id': course_id})
 
     assert response.status_code == 200
     data = response.json()
+    assert data[0]['id'] == course_id
+    assert data[0]['name'] == course_name
 
 
+@pytest.mark.django_db
+def test_filter_courses_by_id(client, student_factory, course_factory):
+    students = student_factory(_quantity=5)
+    courses = course_factory(_quantity=3, students=students)
+    course1_id = courses[0].id
+    course2_id = courses[1].id
+    course1_name = courses[0].name
+    course2_name = courses[1].name
+
+    response = client.get('/api/v1/courses/', {'id': [course1_id, course2_id]})
+
+    assert response.status_code == 200
+    data = response.json()
+    assert data[0]['name'] == course1_name
+    assert data[1]['name'] == course2_name
+
+
+@pytest.mark.django_db
+def test_filter_courses_by_name(client, student_factory, course_factory):
+    students = student_factory(_quantity=5)
+    courses = course_factory(_quantity=3, students=students)
+    course1_id = courses[0].id
+    course2_id = courses[1].id
+    course1_name = courses[0].name
+    course2_name = courses[1].name
+
+    response = client.get('/api/v1/courses/', {'name': [course1_name, course2_name]})
+
+    assert response.status_code == 200
+    data = response.json()
+    assert data[0]['id'] == course1_id
+    assert data[1]['id'] == course2_id
+
+
+@pytest.mark.django_db
+def test_create_course(client):
+    course_data = {
+        'name': 'Теоретическая механика',
+        'students': [
+            {
+                'name': 'Даша',
+                'birth_date': '2002-06-03'
+            },
+            {
+                'name': 'Наташа',
+                'birth_date': '2002-07-22'
+            }
+        ]
+    }
+
+    response = client.post('/api/v1/courses/', course_data, format='json')
+
+    assert response.status_code == 201
+
+
+@pytest.mark.django_db
+def test_update_course(client, student_factory, course_factory):
+    students = student_factory(_quantity=5)
+    courses = course_factory(_quantity=3, students=students)
+    course_id = courses[0].id
+    new_data = {
+        'name': 'Теоретическая механика',
+    }
+
+    response = client.patch(f'/api/v1/courses/{course_id}/', new_data, format='json')
+
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_delete_course(client, student_factory, course_factory):
+    students = student_factory(_quantity=5)
+    courses = course_factory(_quantity=3, students=students)
+    course_id = courses[0].id
+
+    response = client.delete(f'/api/v1/courses/{course_id}/', format='json')
+
+    assert response.status_code == 204
